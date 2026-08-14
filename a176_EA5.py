83080eea256ffe43cf153a5a3793062b270a88f5
abhinav <11249a176@kanchiuniv.ac.in>
2026-08-14 16:01:22 +0530

    Update and rename EA5.py to a176_EA5.py

--- a176_EA5.py
+++ a176_EA5.py
@@ -1,7 +1,5 @@
 import random
-
 Customer = ["User"]
 Action = ["browses products", "adds to cart", "places order", "makes payment"]
-
 for i in range(5):
     print(random.choice(Customer), random.choice(Action))
